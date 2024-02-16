a4e53cfc5b324fa89203cb19ddd73d4921ad68f2
p4ulweiss <s52856@edu.campus02.at>
2024-02-16 17:08:20 +0100

    Renamed testclasses and added comments in numpy format

--- tests/game_board_test.py
+++ tests/game_board_test.py
@@ -5,12 +5,60 @@ from captains_mistress.gamecomponents.gameboard import GameBoard
 from captains_mistress.gamecomponents.gametoken import GameToken
 
 
-class MyTestCase(unittest.TestCase):
+class TestGameBoard(unittest.TestCase):
+    """
+    Test case for the Connect Four game board.
+
+    This class contains unit tests for the methods of the GameBoard class.
+
+    Methods
+    -------
+    setUp()
+        Set up the test case with a new instance of the GameBoard.
+    test_reset_gameboard()
+        Test the reset_gameboard method of the GameBoard class.
+    test_drop_token_valid_column()
+        Test the drop_token method with a valid column index.
+    test_drop_token_full_column()
+        Test the drop_token method with a full column.
+    test_drop_token_invalid_column_index()
+        Test the drop_token method with an invalid column index.
+    test_no_winner()
+        Test the check_winner method when there is no winner.
+    test_horizontal_winner()
+        Test the check_winner method for a horizontal winner.
+    test_vertical_winner()
+        Test the check_winner method for a vertical winner.
+    test_diagonal_winner()
+        Test the check_winner method for a diagonal winner.
+    test_no_winner_partial_board()
+        Test the check_winner method when the game is not over.
+
+    """
+
     @classmethod
     def setUp(self):
+        """
+        Set up the test case with a new instance of the GameBoard.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         self.game_board = GameBoard(6, 7)
 
     def test_reset_gameboard(self):
+        """
+        Test the reset_gameboard method of the GameBoard class.
+
+        This method checks if the game board is reset to all empty tokens.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         self.game_board.reset_gameboard()
         # Check if the board is reset to all empty tokens
         for row in self.game_board.board:
@@ -18,9 +66,25 @@ class MyTestCase(unittest.TestCase):
                 self.assertEqual(token, GameToken.EMPTY)
 
     def test_drop_token_valid_column(self):
+        """
+        Test the drop_token method with a valid column index.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         self.assertTrue(self.game_board.drop_token(0, GameToken.RED))
 
     def test_drop_token_full_column(self):
+        """
+        Test the drop_token method with a full column.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         # Fill a column with tokens
         for _ in range(self.game_board.rows):
             self.game_board.drop_token(0, GameToken.YELLOW)
@@ -28,26 +92,66 @@ class MyTestCase(unittest.TestCase):
             self.game_board.drop_token(0, GameToken.RED)
 
     def test_drop_token_invalid_column_index(self):
+        """
+        Test the drop_token method with an invalid column index.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         # Test dropping a token into an invalid column index
         with self.assertRaises(IndexError):
             self.game_board.drop_token(7, GameToken.RED)
 
     def test_no_winner(self):
+        """
+        Test the check_winner method when there is no winner.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         self.assertFalse(self.game_board.check_winner())
 
     def test_horizontal_winner(self):
+        """
+        Test the check_winner method for a horizontal winner.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         # Test horizontal winner
         for col in range(4):
             self.game_board.drop_token(col, GameToken.RED)
         self.assertTrue(self.game_board.check_winner())
 
     def test_vertical_winner(self):
+        """
+        Test the check_winner method for a vertical winner.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         # Test vertical winner
         for _ in range(4):
             self.game_board.drop_token(0, GameToken.YELLOW)
         self.assertTrue(self.game_board.check_winner())
 
     def test_diagonal_winner(self):
+        """
+        Test the check_winner method for a diagonal winner.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         # Test diagonal winner
         for i in range(4):
             for j in range(4):
@@ -56,6 +160,14 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(self.game_board.check_winner())
 
     def test_no_winner_partial_board(self):
+        """
+        Test the check_winner method when the game is not over.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         # Test when the game is not over and there is no winner yet
         self.game_board.drop_token(0, GameToken.RED)
         self.game_board.drop_token(1, GameToken.YELLOW)
--- tests/game_engine_test.py
+++ tests/game_engine_test.py
@@ -1,16 +1,44 @@
 import unittest
-
-from captains_mistress.gamecomponents.gametoken import GameToken
 from captains_mistress.game_engine import GameEngine
+from captains_mistress.gamecomponents.gametoken import GameToken
 
+class TestGameEngine(unittest.TestCase):
+    """
+    Test case for the Connect Four game engine.
 
-class TestGameLogic(unittest.TestCase):
+    This class contains unit tests for the methods of the GameEngine class.
+
+    Methods
+    -------
+    setUp()
+        Set up the test case with a new instance of the GameEngine.
+    test_switch_player()
+        Test the switch_player method of the GameEngine class.
+    """
+
     @classmethod
     def setUp(self):
+        """
+        Set up the test case with a new instance of the GameEngine.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         self.game_engine = GameEngine()
 
-    #testing player switch method
     def test_switch_player(self):
+        """
+        Test the switch_player method of the GameEngine class.
+
+        This method tests the behavior of switching players.
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         self.game_engine.current_player = GameToken.RED
         self.game_engine.switch_player()
         self.assertEqual(self.game_engine.current_player, GameToken.YELLOW)
